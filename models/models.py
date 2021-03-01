90a934e6a77012d93021dfeab5f5061f553cbc85
apoluden <artiomnkkm@gmail.com>
2021-03-01 14:06:09 +0200

    5th stage all

--- models/models.py
+++ models/models.py
@@ -5,6 +5,31 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    rent_days = fields.Integer(string='Rent days', required=True, default=1)
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm()
+        current_datetime = datetime.datetime.now()
+        BikeRent = self.env['bike.rent']
+        for order_line in self.order_line:
+            BikeRent.create({
+                'bike_id': order_line.product_id.id,
+                'price': order_line.price_unit,
+                'partner_id': self.partner_id.id,
+                'rent_start': current_datetime,
+                'rent_stop': current_datetime + datetime.timedelta(days=order_line.rent_days)
+            })
+
+
 class Partner(models.Model):
 
     _inherit = 'res.partner'
